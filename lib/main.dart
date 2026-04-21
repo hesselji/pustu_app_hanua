481df950e004da275a1fbfe1ae0434bce376d62a
hesselji <josefimanuelhessel@gmail.com>
2026-04-21 23:41:06 +0700

    change import

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'screens/home_screen.dart';
+import 'screens/mobile/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
