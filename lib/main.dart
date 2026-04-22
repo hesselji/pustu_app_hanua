6a8a1f17e7cf11f30a5b54cfddf8b87c77d0af99
Bagus Rian B <magusrian09@gmail.com>
2026-04-22 19:53:22 +0700

    InfoToggle_Pelayanan

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+
 import 'screens/home_screen.dart';
+import 'provider/service_status.dart'; // ✅ FIX
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => ServiceStatus(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
