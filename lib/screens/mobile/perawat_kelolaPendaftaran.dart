db52f63e9370d526f1e45a2b61fbcb74c1ff933b
Mahendra Juliansen <m357439@gmail.com>
2026-04-27 19:07:34 +0700

    Penyempurnaan Menu Kelola Pendaftaran

--- lib/screens/mobile/perawat_kelolaPendaftaran.dart
+++ lib/screens/mobile/perawat_kelolaPendaftaran.dart
@@ -105,7 +105,7 @@ class _PerawatKelolaPendaftaranScreenState
       List<QueryDocumentSnapshot> hasil = [];
 
       for (var doc in snapshot.docs) {
-        final data = doc.data() as Map<String, dynamic>;
+        final data = doc.data();
 
         if (data["tanggal"] == null) continue;
 
