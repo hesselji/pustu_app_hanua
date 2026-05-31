743c2a48b3e2ef5c19122d57e0f225b4b90cb5fd
hesselji <josefimanuelhessel@gmail.com>
2026-06-01 06:41:44 +0700

    cleaning code

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:provider/provider.dart';
 import 'dart:ui';
-
 import 'provider/service_status.dart';
 import 'firebase_options.dart';
 import 'screens/desktop/web_home_screen.dart';
